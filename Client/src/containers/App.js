8a407e4f9edc7d37b2fb9032b73289bcf5340631
Ryan Cole <rcole5@myune.edu.au>
2017-10-22 16:34:26 +1100

    Changed to rc-slider

--- Client/src/containers/App.js
+++ Client/src/containers/App.js
@@ -1,62 +1,84 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
-import Slider from 'react-rangeslider';
+import 'rc-slider/assets/index.css';
+import Slider from 'rc-slider';
+import './App.css';
 
 class App extends Component {
-  constructor(props, context) {
-    super(props, context)
-    this.state = {
-      volume: 100,
-      url: 'http://192.168.1.9:8080'
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            volume: 100,
+            mute: false,
+            url: 'http://192.168.1.9:8080'
+        }
     }
-  }
 
-  handleOnChange = (value) => {
-    console.log(value);
-    this.setState({
-      volume: value
-    })
-    const self = this;
-    axios.get(self.state.url + '/volume/' + self.state.volume).then(function(response) {
-      console.log(response.data.data)
-    }).catch(function(response){
-    });
-  }
+    handleOnChange = (value) => {
+        this.setState({
+            volume: value
+        });
 
-  handleChangeFinish = (val) => {
-    console.log(val);
-  }
+        const self = this;
+        axios.get(self.state.url + '/volume/' + self.state.volume).then(function (response) {
+            self.setState({
+                mute: response.data.data.muted
+            });
+        }).catch(function (response) {
+        });
+    };
 
-  handleMute = () => {
-    const self = this;
-    axios.get(self.state.url + '/mute').then(function(response) {
+    handleChangeFinish = (val) => {
+        const self = this;
+        axios.get(self.state.url + '/volume/' + self.state.volume).then(function (response) {
+        }).catch(function (response) {
+        });
+    };
 
-    });
-  }
+    handleMute = () => {
+        const self = this;
 
-  componentDidMount() {
-    const self = this;
-    axios.get(self.state.url).then(function(response){
-      self.setState({volume: response.data.data.volume});
-    });
-  }
+        axios.get(self.state.url + '/mute').then(function (response) {
+            self.setState({mute: !self.state.mute});
+        });
 
-  render() {
-    let { volume } = this.state
-    return (
-      <div>
-      <h3 style={{textAlign: 'center'}}>{this.state.volume}</h3>
-      <Slider
-        value={volume}
-        orientation="vertical"
-        step={1}
-        onChange={this.handleOnChange}
-        onChangeComplete={this.handleChangeFinish}
-      />
-      <center><button onClick={this.handleMute}>Mute</button></center>
-      </div>
-    )
-  }
+    };
+
+    componentDidMount() {
+        const self = this;
+        axios.get(self.state.url).then(function (response) {
+            self.setState({
+                volume: response.data.data.volume,
+                mute: response.data.data.muted
+            });
+        });
+    }
+
+    render() {
+        const style = {width: 100 + '%', height: 400, display: 'flex', justifyContent: 'center'};
+        const centerStyle = {display: 'flex', justifyContent: 'center'};
+        return (
+            <div>
+
+                <h3 style={{textAlign: 'center'}}>{this.state.volume}</h3>
+                <div style={style}>
+                    <Slider
+                        vertical
+                        min={0}
+                        max={100}
+                        onChange={this.handleOnChange}
+                        onAfterChange={this.handleChangeFinish}
+                        defaultValue={50}
+                        value={this.state.volume}
+                    />
+                </div>
+                <div style={centerStyle}>
+                    <button className="mute-btn"
+                            onClick={this.handleMute}>{this.state.mute ? "Unmute" : "Mute" }</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
