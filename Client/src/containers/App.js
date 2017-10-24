0280e9f6d30a29d7c8ef2403900f8b92c39b2804
Ryan Cole <rcole5@myune.edu.au>
2017-10-24 14:28:10 +1100

    Added error messages

--- Client/src/containers/App.js
+++ Client/src/containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import 'rc-slider/assets/index.css';
+import AlertContainer from 'react-alert';
 import Slider from 'rc-slider';
 import './App.css';
 
@@ -11,7 +12,15 @@ class App extends Component {
             volume: 100,
             mute: false,
             url: 'http://192.168.1.9:8080'
-        }
+        };
+
+        this.alertOptions = {
+            offset: 14,
+            position: 'top left',
+            theme: 'dark',
+            time: 5000,
+            transition: 'scale'
+        };
     }
 
     handleOnChange = (value) => {
@@ -25,6 +34,8 @@ class App extends Component {
                 mute: response.data.data.muted
             });
         }).catch(function (response) {
+            self.msg.removeAll();
+            self.msg.error("Couldn't connect so server.")
         });
     };
 
@@ -32,34 +43,40 @@ class App extends Component {
         const self = this;
         axios.get(self.state.url + '/volume/' + self.state.volume).then(function (response) {
         }).catch(function (response) {
+            self.msg.removeAll();
+            self.msg.error("Couldn't connect so server.")
         });
     };
 
     handleMute = () => {
         const self = this;
-
         axios.get(self.state.url + '/mute').then(function (response) {
             self.setState({mute: !self.state.mute});
+        }).catch(function (response) {
+            self.msg.removeAll();
+            self.msg.error("Couldn't connect so server.")
         });
-
     };
 
-    componentDidMount() {
+    componentDidMount = () => {
         const self = this;
         axios.get(self.state.url).then(function (response) {
             self.setState({
                 volume: response.data.data.volume,
                 mute: response.data.data.muted
             });
+        }).catch(function (response){
+            self.msg.error("Couldn't connect so server.")
         });
-    }
+    };
+
 
     render() {
         const style = {width: 100 + '%', height: 400, display: 'flex', justifyContent: 'center'};
         const centerStyle = {display: 'flex', justifyContent: 'center'};
         return (
             <div>
-
+                <AlertContainer ref={a => this.msg = a} {...this.alertOptions}/>
                 <h3 style={{textAlign: 'center'}}>{this.state.volume}</h3>
                 <div style={style}>
                     <Slider
